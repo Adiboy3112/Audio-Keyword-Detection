474bec3dde2bf80806e738ef1ccfec79304538c4
Your Name <adiboy_3112@kgpian.iitkgp.ac.in>
2022-06-10 02:48:47 +0530

    Added custom header file containing trained model ready to be deployed

--- nano_ble33_sense_microphone/nano_ble33_sense_microphone.ino
+++ nano_ble33_sense_microphone/nano_ble33_sense_microphone.ino
@@ -1,6 +1,23 @@
 
 // If your target is limited in memory remove this macro to save 10K RAM
 #define EIDSP_QUANTIZE_FILTERBANK   0
+
+/*
+ ** NOTE: If you run into TFLite arena allocation issue.
+ **
+ ** This may be due to may dynamic memory fragmentation.
+ ** Try defining "-DEI_CLASSIFIER_ALLOCATION_STATIC" in boards.local.txt (create
+ ** if it doesn't exist) and copy this file to
+ ** `<ARDUINO_CORE_INSTALL_PATH>/arduino/hardware/<mbed_core>/<core_version>/`.
+ **
+ ** See
+ ** (https://support.arduino.cc/hc/en-us/articles/360012076960-Where-are-the-installed-cores-located-)
+ ** to find where Arduino installs cores on your machine.
+ **
+ ** If the problem persists then there's not enough memory for this model and application.
+ */
+
+/* Includes ---------------------------------------------------------------- */
 #include <PDM.h>
 #include <keyword-detection_inferencing.h>
 
